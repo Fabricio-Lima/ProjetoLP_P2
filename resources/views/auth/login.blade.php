2675583bf89ba063c8abc071eafe5bf0966a8150
Fabricio de Lima Costa <73128869+Fabricio-Lima@users.noreply.github.com>
2022-11-06 00:09:26 -0300

    alterando views auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,6 +38,9 @@
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif-->
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('Não possuí cadastro? Cadastre-se aqui') }}
+                </a>
 
                 <x-jet-button class="ml-4">
                     {{ __('Entrar') }}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,28 +10,28 @@
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('Nome') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('nome')" required autofocus autocomplete="nome" />
+                <x-jet-label for="nome" value="{{ __('Nome') }}"/>
+                <x-jet-input id="nome" class="block mt-1 w-full" type="text" name="nome" :value="old('nome')" required autofocus autocomplete="nome" />
             </div>
 
-            <div>
+            <div class="mt-4">
                 <x-jet-label for="cpf" value="{{ __('CPF') }}" />
-                <x-jet-input id="cpf" class="block mt-1 w-full" type="number_format" name="cpf" :value="old('cpf')" required autofocus autocomplete="cpf" />
+                <x-jet-input id="cpf" class="block mt-1 w-full" type="number" name="cpf" :value="old('cpf')" required autofocus autocomplete="cpf" />
             </div>
 
-            <div>
+            <div class="mt-4">
                 <x-jet-label for="rg" value="{{ __('RG') }}" />
-                <x-jet-input id="rg" class="block mt-1 w-full" type="number_format" name="rg" :value="old('rg')" required autofocus autocomplete="rg" />
+                <x-jet-input id="rg" class="block mt-1 w-full" type="text" name="rg" :value="old('rg')" required autofocus autocomplete="rg" />
             </div>
 
-            <div>
+            <div class="mt-4">
                 <x-jet-label for="celular" value="{{ __('Celular') }}" />
-                <x-jet-input id="celular" class="block mt-1 w-full" type="number_format" name="celular" :value="old('celular')" required autofocus autocomplete="celular" />
+                <x-jet-input id="celular" class="block mt-1 w-full" type="text" name="celular" :value="old('celular')" required autofocus autocomplete="celular" />
             </div>
 
-            <div>
+            <div class="mt-4">
                 <x-jet-label for="telefone" value="{{ __('Telefone') }}" />
-                <x-jet-input id="telefone" class="block mt-1 w-full" type="number_format" name="telefone" :value="old('telefone')" required autofocus autocomplete="telefone" />
+                <x-jet-input id="telefone" class="block mt-1 w-full" type="text" name="telefone" :value="old('telefone')" required autofocus autocomplete="telefone" />
             </div>
 
             <div class="mt-4">
@@ -40,22 +40,22 @@
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-jet-label for="password" value="{{ __('Senha') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password"/>
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-jet-label for="password_confirmation" value="{{ __('Confirmar senha') }}" />
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Já possuí cadastro?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Registrar') }}
                 </x-jet-button>
             </div>
         </form>
