f4aec6d449a22b717c307877596c8a8ad84c1df7
Fabricio de Lima Costa <73128869+Fabricio-Lima@users.noreply.github.com>
2022-11-09 13:51:43 -0300

    adiciona model produto

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'nome',
+        'preco',
+        'fornecedor_id',
+        'categoria_id'
+    ];
+  
+    public function Provider()
+    {
+        return $this->belongsTo('App\Models\Provider', 'fornecedor_id');
+    }
+  
+     public function Category()
+    {
+        return $this->belongsTo('App\Models\Category', 'categoria_id');
+    }
+}
