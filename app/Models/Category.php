fe39c0bf481af852a6f12ba668326883d98a1fe4
Fabricio de Lima Costa <73128869+Fabricio-Lima@users.noreply.github.com>
2022-11-09 22:13:13 -0300

    adiciona model category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Task extends Model
+class Category extends Model
 {
     use HasFactory, SoftDeletes;
 
